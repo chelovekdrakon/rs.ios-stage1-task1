d91e4b17c4efc43874df9ec4ad9fdb5ec29604db
Fiodar Morau <chelovekdrakon@gmail.com>
2020-03-17 19:55:22 +0300

    feature: Exercise 2

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,44 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    int minIndex = 0;
+    int maxIndex = 0;
+    
+    for (int i = 1; i < array.count; i++) {
+        int minValue = [array[minIndex] intValue];
+        int maxValue = [array[maxIndex] intValue];
+        
+        int value = [array[i] intValue];
+        
+        if (value < minValue) {
+            minIndex = i;
+        }
+        
+        if (value > maxValue) {
+            maxIndex = i;
+        }
+    }
+    
+    NSMutableArray *minArray = [array mutableCopy];
+    [minArray removeObjectAtIndex:maxIndex];
+    NSNumber *minSum = [self sumArrayValues:minArray];
+    
+    NSMutableArray *maxArray = [array mutableCopy];
+    [maxArray removeObjectAtIndex:minIndex];
+    NSNumber *maxSum = [self sumArrayValues:maxArray];
+    
+    
+    return @[minSum, maxSum];
+}
+
+- (NSNumber *)sumArrayValues:(NSArray<NSNumber*>*)array {
+    int sum = 0;
+    
+    for (NSNumber *number in array) {
+        sum += [number intValue];
+    }
+    
+    return @(sum);
 }
 
 @end
