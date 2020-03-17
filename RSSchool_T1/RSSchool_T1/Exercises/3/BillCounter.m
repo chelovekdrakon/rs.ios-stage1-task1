95510fd42e1900a64de9b5a6ee2b9f5acc6fa5a1
Fiodar Morau <chelovekdrakon@gmail.com>
2020-03-17 20:03:11 +0300

    feature: Exercise 3

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,22 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    NSMutableArray *excludedAllergic = [bill mutableCopy];
+    [excludedAllergic removeObjectAtIndex:index];
+    
+    int sumToSplit = 0;
+    
+    for (NSNumber *price in excludedAllergic) {
+        sumToSplit += [price intValue];
+    }
+    
+    int annaBill = sumToSplit / 2;
+    
+    if ([sum isEqualToNumber:@(annaBill)]) {
+        return @"Bon Appetit";
+    } else {
+        return [NSString stringWithFormat: @"%i", ([sum intValue] - annaBill)];
+    }
 }
 
 @end
