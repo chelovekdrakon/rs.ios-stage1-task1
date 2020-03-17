34b74e3de0e8c5a60d982ddc83875fa30cb8aa0a
Fiodar Morau <chelovekdrakon@gmail.com>
2020-03-17 22:41:36 +0300

    feature: Exercise 4

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,46 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    NSMutableArray *result = [NSMutableArray array];
+    
+    NSDictionary *openedBracketPairs = @{
+        @"(": @")",
+        @"[": @"]",
+        @"<": @">",
+    };
+    
+    NSMutableArray *openedBracketsList = [NSMutableArray array];
+    NSMutableArray *openedBracketsIndexes = [NSMutableArray array];
+    
+    for (int i = 0; i < string.length; i++) {
+        NSString *symbol = [string substringWithRange:NSMakeRange(i, 1)];
+        
+        for (int j = (int)openedBracketsList.count; (j > 0); j--) {
+            int bracketStackIndex = j - 1;
+            NSString *expectedSymbol = [openedBracketsList objectAtIndex:bracketStackIndex];
+            
+            if ([symbol isEqualToString:expectedSymbol]) {
+                int openBracketIndex = [[openedBracketsIndexes objectAtIndex:bracketStackIndex] intValue];
+                int firstLetterIndex = openBracketIndex + 1;
+                NSString *substring = [string substringWithRange:NSMakeRange(firstLetterIndex, i - firstLetterIndex)];
+                [result addObject:substring];
+            
+                [openedBracketsList removeObjectAtIndex:bracketStackIndex];
+                [openedBracketsIndexes removeObjectAtIndex:bracketStackIndex];
+                // match found
+                break;
+            }
+        }
+        
+        NSString *closingBracket = [openedBracketPairs objectForKey:symbol];
+        
+        if (closingBracket) {
+            [openedBracketsList addObject:closingBracket];
+            [openedBracketsIndexes addObject:@(i)];
+        }
+    }
+    
+    return result;
 }
 
 @end
