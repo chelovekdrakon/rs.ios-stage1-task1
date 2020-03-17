3988579a94ded425fa625d1d64fa09fef6946447
Fiodar Morau <chelovekdrakon@gmail.com>
2020-03-17 19:37:17 +0300

    feature: Exercise 1

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,23 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    if (!sadArray.count) {
+        return sadArray;
+    }
+    
+    NSMutableArray* result = [[NSMutableArray alloc] initWithArray:sadArray];
+    
+    for (int i = 1; i < result.count - 1 ; i++) {
+        int value = [result[i] intValue];
+        int neighbors = [result[i - 1] intValue] + [result[i + 1] intValue];
+        
+        if (value > neighbors) {
+            [result removeObjectAtIndex: i];
+            i = i > 1 ? (i - 2) : (i - 1);
+        }
+    }
+    
+    return (NSArray*)result;
 }
 
 @end
